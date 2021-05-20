8c4e376cf73d2c621407401ad020133929c071aa
Lars Lenckowski <larslenckowski@gmail.com>
2021-05-20 16:37:29 +0200

    change coordinate mapping to make later changes (raycasting instead of rendering the whole thing) easier

--- src/compositor.rs
+++ src/compositor.rs
@@ -13,17 +13,9 @@ use std::{
 pub const SEG_SIZE: usize = 16 * 8;
 pub const CHUNK_HEIGHT: usize = 255;
 
-fn zseg_index_to_xyz(n: usize) -> (i32, i32, i32) {
-    let (x, z, y) = (
-        n % SEG_SIZE,
-       (n / SEG_SIZE) % SEG_SIZE,
-        n / SEG_SIZE / SEG_SIZE
-    );
-
-    (x as i32, y as i32, z as i32)
-}
-
 pub fn render_segment(name: &str) {
+    // has to be a vector because otherwise it is bigger than the stack. could maybe
+    // be a box of an array instead, if that is faster
     let mut seg = vec![[[0xFFFFu16; SEG_SIZE]; SEG_SIZE]; 255];
     let mut textures: HashMap<u16, ImageBuffer<Rgba<u8>, Vec<u8>>> = HashMap::new();
 
@@ -54,7 +46,7 @@ pub fn render_segment(name: &str) {
                     let block_id = zseg.read_u16::<NativeEndian>().unwrap() & 0x7fff;
                     let (x, y, z) = zseg_index_to_xyz(c);
                     seg[y as usize][x as usize][z as usize] = block_id;
-                    c += 1
+                    c += 1;
                 }
                 Err(_e) => break,
             }
@@ -67,14 +59,16 @@ pub fn render_segment(name: &str) {
     );
 
     for y in 0..CHUNK_HEIGHT {
-        for x in (0..SEG_SIZE).rev() {
-            for z in (0..SEG_SIZE).rev() {
+        for x in 0..SEG_SIZE {
+            for z in 0..SEG_SIZE {
                 match seg[y][x][z] {
                     0xFFFF => (),
                     103 => (),
                     block_id => {
                         // println!("{}-{}-{}: {}\x1b[A\r", x, y, z, block_id);
-                        let coords = isometric_coord_mapping(x, y, z);
+                        let coords = isometric_coord_mapping(
+                            x as i32, y as i32, z as i32
+                        );
                         let texture = textures.get(&block_id).expect("asdasd");
                         image_buffer_blit(&mut view, texture, coords);
                     }
@@ -114,12 +108,28 @@ pub fn image_buffer_blit(
     }
 }
 
-pub fn isometric_coord_mapping(x: usize, y: usize, z: usize) -> (u32, u32) {
-    let (x, y, z) = (x as i32, y as i32, z as i32);
-    let (x, y, z) = (x * 16, y * 16, z * 16);
-    let (sx, sy) = (
-        (SEG_SIZE * 16) as i32 / 2 + (x - z) / 2,
-        ((SEG_SIZE as i32 / 2 + CHUNK_HEIGHT as i32) * 16) - (y / 2 + (x + z) / 4),
+const fn isometric_coord_mapping(x: i32, y: i32, z: i32) -> (u32, u32) {
+    const BASE_X: i32 = 1020;
+    const BASE_Y: i32 = 4072;
+
+    const XDIFF: (i32, i32) = (-8,  4);
+    const ZDIFF: (i32, i32) = ( 8,  4);
+    const YDIFF: (i32, i32) = ( 0, -8);
+
+    let diff = (XDIFF.0 * x + YDIFF.0 * y + ZDIFF.0 * z,
+                XDIFF.1 * x + YDIFF.1 * y + ZDIFF.1 * z);
+
+    let coords = (BASE_X + diff.0, BASE_Y + diff.1);
+
+    (coords.0 as u32, coords.1 as u32)
+}
+
+const fn zseg_index_to_xyz(n: usize) -> (i32, i32, i32) {
+    let (x, z, y) = (
+        n % SEG_SIZE,
+       (n / SEG_SIZE) % SEG_SIZE,
+        n / SEG_SIZE / SEG_SIZE
     );
-    (sx as u32, sy as u32)
+
+    (x as i32, y as i32, z as i32)
 }
