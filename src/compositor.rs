548828e4391427632afcce9369a996a1793c1ad8
Lars Lenckowski <larslenckowski@gmail.com>
2021-05-21 09:32:37 +0200

    remove need for bounds check, and render blocks during parsing for performance

--- src/compositor.rs
+++ src/compositor.rs
@@ -10,7 +10,7 @@ const CHUNK_HEIGHT: usize = 256;
 pub fn render_segment(name: &str) {
     // seg has to be a vector because otherwise it is bigger than the stack. could
     // maybe be a box of an array instead, if that is faster
-    let mut seg = vec![[[0xFFFFu16; SEG_SIZE]; SEG_SIZE]; CHUNK_HEIGHT];
+    //let mut seg = vec![[[0xFFFFu16; SEG_SIZE]; SEG_SIZE]; CHUNK_HEIGHT];
     let mut textures: HashMap<u16, ImageBuffer<Rgba<u8>, Vec<u8>>> = HashMap::new();
 
     let mut segrd = SegmentReader::new(name);
@@ -19,32 +19,18 @@ pub fn render_segment(name: &str) {
         textures.insert(block_id, isometric);
     }
 
-    for (x, y, z, block_id) in segrd.iter_blocks() {
-        seg[y as usize][x as usize][z as usize] = block_id;
-    }
-
     let mut view: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(
         16 * SEG_SIZE as u32,
         16 * (SEG_SIZE as u32 + CHUNK_HEIGHT as u32) / 2,
     );
 
-    for y in 0..CHUNK_HEIGHT {
-        for x in 0..SEG_SIZE {
-            for z in 0..SEG_SIZE {
-                match seg[y][x][z] {
-                    0xFFFF => (),
-                    103 => (),
-                    block_id => {
-                        // println!("{}-{}-{}: {}\x1b[A\r", x, y, z, block_id);
-                        let coords = isometric_coord_mapping(
-                            x as i32, y as i32, z as i32
-                        );
-                        let texture = textures.get(&block_id).expect("asdasd");
-                        image_buffer_blit(&mut view, texture, coords);
-                    }
-                }
-            }
-        }
+    for (x, y, z, block_id) in segrd.iter_blocks() {
+        //seg[y as usize][x as usize][z as usize] = block_id;
+        let coords = isometric_coord_mapping(
+            x as i32, y as i32, z as i32
+        );
+        let texture = textures.get(&block_id).expect("asdasd");
+        image_buffer_blit(&mut view, texture, coords);
     }
 
     view.save(format!("public/prerendered/{}.png", name))
@@ -57,25 +43,23 @@ fn image_buffer_blit(
     offset: (u32, u32),
 ) {
     for (x, y, source_pixel) in source.enumerate_pixels() {
-        if target.in_bounds(x + offset.0, y + offset.1) {
-            let target_pixel = target.get_pixel_mut(x + offset.0, y + offset.1);
-            let sa = source_pixel.0[3] as u16;
-            let new_pixel = Rgba::from([
-                ((target_pixel.0[0] as u16 * (255 - sa)) / 255
-                    + (source_pixel.0[0] as u16 * sa) / 255) as u8,
-                ((target_pixel.0[1] as u16 * (255 - sa)) / 255
-                    + (source_pixel.0[1] as u16 * sa) / 255) as u8,
-                ((target_pixel.0[2] as u16 * (255 - sa)) / 255
-                    + (source_pixel.0[2] as u16 * sa) / 255) as u8,
-                255 - (((255 - target_pixel.0[3] as u16) * (255 - sa)) / 255) as u8,
-            ]);
-            *target_pixel = new_pixel;
-        }
+        let target_pixel = target.get_pixel_mut(x + offset.0, y + offset.1);
+        let sa = source_pixel.0[3] as u16;
+        let new_pixel = Rgba::from([
+            ((target_pixel.0[0] as u16 * (255 - sa)) / 255
+                + (source_pixel.0[0] as u16 * sa) / 255) as u8,
+            ((target_pixel.0[1] as u16 * (255 - sa)) / 255
+                + (source_pixel.0[1] as u16 * sa) / 255) as u8,
+            ((target_pixel.0[2] as u16 * (255 - sa)) / 255
+                + (source_pixel.0[2] as u16 * sa) / 255) as u8,
+            255 - (((255 - target_pixel.0[3] as u16) * (255 - sa)) / 255) as u8,
+        ]);
+        *target_pixel = new_pixel;
     }
 }
 
 const fn isometric_coord_mapping(x: i32, y: i32, z: i32) -> (u32, u32) {
-    const BASE_X: i32 = 1020;
+    const BASE_X: i32 = 1016;
     const BASE_Y: i32 = 2040;
 
     const XDIFF: (i32, i32) = (-8,  4);
